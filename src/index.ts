851cebb34e8f8ff558bb5558a87ebb9e62cd4101
Andre <andre@tos.world>
2024-09-04 19:27:25 -0300

    change reactivity to @vue/reactivity

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+export * from "@vue/reactivity";
+
 export * from "./app";
 export * from "./block";
 export * from "./context";
 export * from "./directive";
 export * from "./eval";
-export * from "./reactivity";
+//export * from "./reactivity";
 export * from "./scheduler";
 export * from "./walk";
