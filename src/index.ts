3e8587e6a198d8ef0e36dc675adae9552fc54b37
Andre <andre@tos.world>
2024-09-16 10:23:16 -0300

    fix: import component

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "@vue/reactivity";
 
 export * from "./app";
 export * from "./block";
+export * from "./component";
 export * from "./context";
 export * from "./directive";
 export * from "./eval";
